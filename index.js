8cf988abfb5c8bd405d092721f5f5f7dd1d7c764
Peter Seremetis <pswriter5348@gmail.com>
2018-03-02 18:58:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 function dwarfRollCall(dwarves) {
-}
+  let i = 0;
+  while (i < dwarves.length){
+  dwarves[i] = ( `${[i + 1]}. `+dwarves[i]);
+  i++;
+  }
+     return (`${dwarves.join(' ')} `)
+  }
+
 
 function summonCaptainPlanet(planeteerCalls){
+  for(let i = 0; i < planeteerCalls.length; i++){
+       planeteerCalls[i] = (planeteerCalls[i]+ `!`);
+    }
+    return planeteerCalls
 }
+
 
 function longPlaneteerCalls(words) {
+  if (words.length > 4){
+    return true
+  }else{
+    return false
+  }
 }
 
+
 function findTheCheese (foods) {
+var cheese = foods.find(function(element){
+  return element === `cheddar`
+});
+if(cheese === `cheddar`){
+  return (`${cheese}`)
+}else{
+  return (`no cheese!`)
+  }
 }
